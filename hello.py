25b9c4142c9914866bb8ec36861de38064a5eff7
Riad Hak <99054420@mydavinci.nl>
2022-02-23 10:55:39 +0100

    update

---
+++ hello.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from turtle import position
+
+window = tk.Tk()
+window.title('Hello')
+window.geometry('300x300')
+label1 = tk.Label(
+    window,
+    text = 'Hello\nTkinter!',
+    bg='green',
+    fg= 'yellow',
+    font=("Helvetica", 30),
+    width=10,
+    height=2,
+    
+)
+
+
+label1.place(
+    relx = 0.5,
+    rely = 0.5,anchor = 'center'
+)
+
+window.mainloop()
